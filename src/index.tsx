b2110e811cf40e408ed6f680fb326fe5ea114b32
leandro.tavares <leandro.tavares@original.com.br>
2021-12-20 20:57:46 -0300

    corrige nome e importaçao

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import './styles/reset.scss';
+import './styles/pokemon-colors.scss';
 
 ReactDOM.render(
   <React.StrictMode>
